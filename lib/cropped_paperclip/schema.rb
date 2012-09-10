e47358dbe5e978f3977fbad53f0522b55b55bf1f
William Ross <will@spanner.org>
2012-09-10 11:03:38 +0100

    migration support updated for paperclip 3.1.x, dependency changed.

--- lib/cropped_paperclip/schema.rb
+++ lib/cropped_paperclip/schema.rb
@@ -3,49 +3,30 @@ module CroppedPaperclip
   # Copied from, and often makes calls on, the equivalent file in Paperclip.
   #
   module Schema
-    @@upload_columns = {:upload_id => :integer,
-                 :scale_width => :integer,
-                 :scale_height => :integer,
-                 :offset_left => :integer,
-                 :offset_top => :integer,
-                 :version => :integer}
+    UPLOAD_COLUMNS = {
+      :file_name    => :string,
+      :content_type => :string,
+      :file_size    => :integer,
+      :updated_at   => :datetime,
+      :upload_id    => :integer,
+      :scale_width  => :integer,
+      :scale_height => :integer,
+      :offset_left  => :integer,
+      :offset_top   => :integer
+    }
 
-    def has_upload(attachment_name)
-      has_attached_file(attachment_name)
-      with_columns_for_upload(attachment_name) do |column_name, column_type|
-        column(column_name, column_type)
-      end
-    end
-    
-    def add_upload(table_name, attachment_name)
-      add_attached_file(table_name, attachment_name)
-      with_columns_for_upload(attachment_name) do |column_name, column_type|
-        add_column(table_name, column_name, column_type)
-      end
+    def self.included(base)
+      ActiveRecord::ConnectionAdapters::Table.send :include, TableDefinition
+      ActiveRecord::ConnectionAdapters::TableDefinition.send :include, TableDefinition
     end
 
-    def drop_upload(table_name, attachment_name)
-      drop_attached_file(table_name, attachment_name)
-      with_columns_for_upload(attachment_name) do |column_name, column_type|
-        remove_column(table_name, column_name)
-      end
-    end
-    
-    # Paperclip doesn't have this one, for some reason.
-    def add_attached_file(table_name, attachment_name)
-      # with_columns_for is defined in Paperclip::Schema
-      with_columns_for(attachment_name) do |column_name, column_type|
-        add_column(table_name, column_name, column_type)
-      end
-    end
-    
-  protected
-
-    def with_columns_for_upload(attachment_name)
-      @@upload_columns.each do |suffix, column_type|
-        # full_column_name is defined in Paperclip::Schema
-        column_name = full_column_name(attachment_name, suffix)
-        yield column_name, column_type
+    module TableDefinition
+      def uploadable_attachment(*attachment_names)
+        attachment_names.each do |attachment_name|
+          UPLOAD_COLUMNS.each_pair do |column_name, column_type|
+            column("#{attachment_name}_#{column_name}", column_type)
+          end
+        end
       end
     end
 

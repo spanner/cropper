7d8d9781fc2bc28277f246595a5aedc8f4a1fe4c
William Ross <will@spanner.org>
2013-02-14 14:55:21 +0000

    add and remove upload columns in migration

--- lib/cropper/schema.rb
+++ lib/cropper/schema.rb
@@ -18,8 +18,32 @@ module Cropper
     def self.included(base)
       ActiveRecord::ConnectionAdapters::Table.send :include, TableDefinition
       ActiveRecord::ConnectionAdapters::TableDefinition.send :include, TableDefinition
+      ActiveRecord::ConnectionAdapters::AbstractAdapter.send :include, Statements
     end
 
+    module Statements
+      def add_upload(table_name, *attachment_names)
+        raise ArgumentError, "Please specify attachment name in your add_upload call in your migration." if attachment_names.empty?
+        
+        attachment_names.each do |attachment_name|
+          UPLOAD_COLUMNS.each_pair do |column_name, column_type|
+            add_column(table_name, "#{attachment_name}_#{column_name}", column_type)
+          end
+        end
+      end
+      
+      
+      def remove_upload(table_name, *attachment_names)
+        raise ArgumentError, "Please specify attachment name in your remove_upload call in your migration." if attachment_names.empty?
+
+        attachment_names.each do |attachment_name|
+          COLUMNS.each_pair do |column_name, column_type|
+            remove_column(table_name, "#{attachment_name}_#{column_name}")
+          end
+        end
+      end
+    end
+    
     module TableDefinition
       def cropped_attachment(*attachment_names)
         attachment_names.each do |attachment_name|
